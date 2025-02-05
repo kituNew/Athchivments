cb8c25279905474026d01c6b26c73d14101e217d
kituNew <vlad640vlad@gmail.com>
2025-02-05 18:14:53 +0300

    Initial Commit

---
+++ Atchivents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atchivents
+//
+//  Created by Zaitsev Vladislav on 05.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AtchiventsTests/AtchiventsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtchiventsTests.swift
+//  AtchiventsTests
+//
+//  Created by Zaitsev Vladislav on 05.02.2025.
+//
+
+import Testing
+@testable import Atchivents
+
+struct AtchiventsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
